a71779ca6633780932b50cbaad2ab7527ee76afc
Jade Hua <jadehua@berkeley.edu>
2015-09-18 10:16:11 +0000

    finished

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,15 @@
 class Foobar
   # Q4 CODE HERE
+  #baz that takes in an array of integers as strings, changes each string into an integer, adds two to each number, keeps the even numbers only, takes out any duplicates, rejects the resulting numbers greater than ten, and returns the sum of the resulting array.
+
+  def self.baz(input)
+  	sum = 0 
+  	input.map { |elem| elem.to_i + 2}.select{ |elem| elem % 2 == 0}.uniq.select{|elem| elem < 10}.each do |elem| 
+  		sum = sum + elem
+  	end
+  	sum
+
+
+  end
+
 end
--- hw1.rb
+++ hw1.rb
@@ -1,17 +1,27 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a + b) ** 2
 end
+# puts squared_sum(2,3)
 
+#This method takes in an array of integers, sorts it, then increments every element by 1, and returns it. It does not matter if this method is destructive or not.
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  a.sort.map {|elem| elem + 1}
 end
+
+# sort_array_plus_one([9,6,8])
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  first_name << " " << last_name
 end
+combine_name("Jade", "Hua")
 
 def blockin_time(a)
   # DO NOT EDIT THIS CODE BELOW
   require './foobar'
   Foobar.baz a
 end
+
+blockin_time ["1", "2", "2", "2", "3", "4", "5", "10"]
